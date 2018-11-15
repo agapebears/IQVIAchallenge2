129a05ad58d5318a54d22faa8fb4aaa5a4f4a380
W <AgapeBears@msn.com>
2018-11-15 13:26:45 -0800

    update Test project

---
+++ IQVIAbadAPI/IQVIAbadAPIMVCApp.Tests/Controllers/TweetControllerTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Web.Mvc;
+using IQVIAbadAPIMVCApp;
+using IQVIAbadAPIMVCApp.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IQVIAbadAPIMVCApp.Tests.Controllers
+{
+    [TestClass]
+    public class TweetControllerTest
+    {
+        [TestMethod]
+        public void TweetsResultsNotNull()
+        {
+            // Arrange
+            TweetController controller = new TweetController();
+
+            // Act
+            ViewResult result = controller.Tweets() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void TweetsAsyncEndDateGreaterThanEqualStartDate()
+        {
+            // Arrange
+            TweetController controller = new TweetController();
+
+            // Act
+            ViewResult result = controller.TweetsAsync() as ViewResult;
+
+            // Assert
+            //Assert.AreEqual("TweetsAsync", result.ViewBag.Title);
+            Assert.IsTrue(result.ViewBag.endDate >= result.ViewBag.startDate);
+
+        }        
+    }
+}
